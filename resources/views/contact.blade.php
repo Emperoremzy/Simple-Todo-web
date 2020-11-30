150e2b755a159bb4765726f42e71cbdb9497af51
edima <edimabassey2@gmail.com>
2020-11-30 10:55:03 -0800

    just a check

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,6 @@
 @extends ('layout')
 @section ('content')
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            <h3>contact</h3>      
+            <h4>contact</h4>      
         </div>
 @endsection
